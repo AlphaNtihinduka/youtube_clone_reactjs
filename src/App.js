b7db9a6785584ddbfc5fdf91ef12670461fdd8da
NTIHINDUKA ALPHA <ntihindukaalpha@yahoo.com>
2023-04-28 22:01:17 +0300

    Route the components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
 
-function App() {
-  return (
-    <h1>React</h1>
+const App = () => (
+    <BrowserRouter>
+      <Box sx={{backgroundColor: '#000'}}>
+        <Navbar/>
+        <Routes>
+          <Route path="/" exact element={<Feed/>} />
+          <Route path="/video/:id" element={<VideoDetail/>} />
+          <Route path="/channel/:id" element={<ChannelDetail/>} />
+          <Route path="/search/:searchTerm" element={<SearchFeed/>} />
+        </Routes>
+      
+      </Box>
+    </BrowserRouter>
   );
-}
 
 export default App;
